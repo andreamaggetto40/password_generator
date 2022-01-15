c43002bb5fd8f92297e10379345236876bf047df
Andrea Maggetto <andreamaggetto40@gmail.com>
2022-01-15 17:50:01 +0100

    Initial commit

---
+++ password_generator.py
@@ -0,0 +1,31 @@
+import random as rand
+
+def init():
+    print("Software used to create a password")
+
+def encryption():
+    string_to_encrypt = input("Insert string to encrypt : \n")
+
+    if(len(string_to_encrypt) > 0):
+        password = ""
+        for i in range(len(string_to_encrypt)):
+            key = int(input("Insert a number between 1 and 9 : \n"))
+
+            if(key >= 1 and key <= 9):
+                helper = ord(string_to_encrypt[i])
+                helper += key
+                password += chr(helper)
+            else:
+                print("An error occured!")
+                break
+        for i in range(len(string_to_encrypt),len(string_to_encrypt) * 2):
+            password += str(rand.randint(len(string_to_encrypt),len(string_to_encrypt) * 2))
+        return password
+    else:
+        print("String length MUST BE greater than 0!")
+
+init()
+password = str(encryption())
+
+passwordFile = open("password.txt","w+")
+passwordFile.write(password)
